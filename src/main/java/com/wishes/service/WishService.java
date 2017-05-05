0efc1041a78b8b31b67f2654c2357d7081283ec9
Vladimir Plotnikov <vladimirp@default-value.com>
2017-05-05 18:01:41 +0300

    - Fix checkStyle.

--- src/main/java/com/wishes/service/WishService.java
+++ src/main/java/com/wishes/service/WishService.java
@@ -15,25 +15,31 @@ import java.util.List;
 @Service("wishService")
 @Transactional
 public class WishService {
+
     @Autowired
     private WishRepository wishRepository;
+
     public Wish findById(int id) {
         return wishRepository.findOne(id);
     }
+
     public  List<Wish> findWishesByUserId(int id) {
         return wishRepository.findWishesByUserId(id);
     }
+
     public void saveWish(Wish wish) {
         wishRepository.save(wish);
     }
-    public void updateWish(Wish wish){
+
+    public void updateWish(Wish wish) {
         Wish entity = findById(wish.getId());
-        if(entity!=null){
+        if(entity != null) {
             entity.setWishes(wish.getWishes());
             entity.setPriority(wish.getPriority());
             entity.setLink(wish.getLink());
         }
     }
+
     public void deleteWishById(int id){
         wishRepository.delete(id);
     }
