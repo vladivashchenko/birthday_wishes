b22930e69a3a564f611bdb57daa4944a2d9840aa
VivaVlad <vlad.ivashenko.98@mail.ru>
2017-05-10 17:03:00 +0300

    Rename DatabaseConfig to AppConfig Configure liquibase properties

---
+++ src/main/java/com/wishes/config/AppConfig.java
@@ -0,0 +1,33 @@
+package com.wishes.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+public class DatabaseConfig {
+    @Bean
+    @ConfigurationProperties("spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Autowired
+    LiquibaseProperties properties;
+
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        properties.setChangeLog("classpath:/db/changelog/db.changelog-master.xml");
+        liquibase.setChangeLog(this.properties.getChangeLog());
+        liquibase.setContexts(this.properties.getContexts());
+        liquibase.setDataSource(this.dataSource());
+        liquibase.setDefaultSchema(this.properties.getDefaultSchema());
+        liquibase.setDropFirst(this.properties.isDropFirst());
+        liquibase.setShouldRun(this.properties.isEnabled());
+        return liquibase;
+    }
+}
